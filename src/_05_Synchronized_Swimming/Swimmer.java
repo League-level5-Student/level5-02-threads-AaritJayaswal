563414138ba1a160a59a5b79793cfbf5f0ee9588
league <aaritrj@gmail.com>
2023-10-29 10:50:12 -0700

    finished level 5 module 2

--- src/_05_Synchronized_Swimming/Swimmer.java
+++ src/_05_Synchronized_Swimming/Swimmer.java
@@ -16,6 +16,10 @@ public class Swimmer extends Thread {
 
 	@Override
 	public void run() {
-		// ...
+		SynchronizedSwimming swim = new SynchronizedSwimming();
+		Swimmer swimmer = new Swimmer(name);
+		for (int i = 0; i < 5; i++) {
+			swim.takeTurn(swimmer);
+		}
 	}
 }
